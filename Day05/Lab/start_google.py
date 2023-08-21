0756754fa0c7bfba92b01c650ce131d60f112f21
Masanori Kikuchi <mkiku97@gmail.com>
2023-08-21 23:03:34 +0900

    commit

---
+++ Day05/Lab/start_google.py
@@ -0,0 +1,6 @@
+## MOVE THIS FILE OFF GITHUB REPO BEFORE SYNCING!
+
+import googlemaps
+
+api_key = "AIzaSyCGAw9e8LbZZLOf-txxxegyBxoZybrKvK8"
+client = googlemaps.Client(api_key)
---
+++ Day05/Lecture/start_google.py
@@ -0,0 +1,6 @@
+## MOVE THIS FILE OFF GITHUB REPO BEFORE SYNCING!
+
+import googlemaps
+
+api_key = ""
+client = googlemaps.Client(api_key)
